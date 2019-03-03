4a8feb74287d672338596e0bbc71c22293a94dfd
Duncan Temple Lang <duncan@r-project.org>
2019-03-03 08:32:34 -0800

    deduplicate functionality

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,8 @@
 
 // use strict;
 
+
+// determine whether u is an element of urls or matches one of the rx elements.
 function inURLs(u, urls, rx) {
 
     var i = 0;
@@ -21,22 +23,30 @@ function inURLs(u, urls, rx) {
     return(false);
 }
 
-function closeTargetTabs()
+
+// close each of the tabs in TargetTabs. This is a hash table
+// indexed by URL and each element is an array of tab objects.
+// so group by URL and potentially multiple tabs.
+function closeTargetTabs(tabs = TargetTabs, leaveOne = LeaveOne)
 {
-    for(var k in TargetTabs) {
-	var tabs = TargetTabs[k];
-	for(var i = LeaveOne ? 1 : 0; i < tabs.length; i++) {
-	    chrome.tabs.remove( tabs[i].id);
+    for(var k in tabs) {
+	var utabs = tabs[k];
+	// if(leaveOne && tabs[k].length > 1) console.log("closing tabs for " + tabs[0].url);
+	for(var i = leaveOne ? 1 : 0; i < utabs.length; i++) {
+	    chrome.tabs.remove( utabs[i].id);
 	}
-	TargetTabs[k] = null;
+	tabs[k] = null;
     }
+    return(tabs);
 }
 
 
+// Move the tabs in TargetTabs to a new window.
 function groupTargetTabs()
 {
     chrome.windows.create({}, function(win) {
 	console.log('Created new window ' + win);
+	//XXX remove the first tab that was created automatically for this new window.
 	for(var k in TargetTabs) {
 	    var tabs = TargetTabs[k];
 	    for(var i = 0; i < tabs.length; i++) {
@@ -49,11 +59,14 @@ function groupTargetTabs()
 }
 
 
+
+
+// Run directly in the body of the page to build the menu.
+// We could defer and let the user select which of the tasks to do and then do these computations.
+
 let utable = document.getElementById('urlTBBody');
-
-var TargetTabs = {};
-var LeaveOne = false;
-
+let TargetTabs = {};
+let LeaveOne = false;
 
 chrome.storage.sync.get(['urls', 'LeaveOne', 'Regexps'], function(data) {
  //   alert('in setting urls ' + data.urls);
@@ -101,10 +114,15 @@ chrome.storage.sync.get(['urls', 'LeaveOne', 'Regexps'], function(data) {
 });
 
 
-document.getElementById('removeTabsButton').addEventListener('click', closeTargetTabs);
+document.getElementById('removeTabsButton').addEventListener('click', function() { TargetTabs = closeTargetTabs(TargetTabs, LeaveOne)});
 
 document.getElementById('groupTabsButton').addEventListener('click', groupTargetTabs);
 
+document.getElementById('mute').addEventListener('click', muteTabs);
+
+document.getElementById('duplicates').addEventListener('click', removeDuplicates);
+
+// arrange for a return (\n) to trigger processRX().
 document.getElementById('rx').addEventListener('keydown', function(event)  {
                                                             if(event.keyCode == 13)
 							        processRX(event.target.value);
@@ -129,6 +147,10 @@ function processRX(rx)
     });    
 }
 
+
+/*
+  Before we create the table's entries, ensure it is empty.
+*/
 function clearTable()
 {
     var tb = document.getElementById("urlTBBody");
@@ -143,13 +165,17 @@ function clearTable()
 
 
 
-function filterTabs(pred)
+/*
+  This is a general version that takes a predicate function and determines if the url matches.
+*/
+function filterTabs(predFun)
 {
         chrome.windows.getAll({populate: true},function(windows){
 	windows.forEach(function(window){
 	    window.tabs.forEach(function(tab){
 		//		console.log(tab.url);
-		if(pred(tab.url)) {  // e.g. pred = function(u) u.match(rx)
+		if(predFun(tab)) {  // e.g. pred = function(tab) tab.url.match(rx)
+		    console.log(tab.url + "  matches");
 		    addURLTabToTable(tab.url, tab);
 		}
 	    });
@@ -159,12 +185,17 @@ function filterTabs(pred)
 
 }
 
+// could just pass tab and get its tab.url !
 function addURLTabToTable(url, tab)
 {
     let tr = document.createElement("tr");
     th = document.createElement("th");
     th.setAttribute('align', 'left');
-    th.innerHTML = url;
+    var a = document.createElement("a");
+    a.innerHTML = url;
+    a.setAttribute('href', url);
+    a.addEventListener('click', function() { showTab(tab); });
+    th.appendChild(a);
     tr.appendChild(th);
     utable.appendChild(tr);
 
@@ -174,4 +205,58 @@ function addURLTabToTable(url, tab)
     tmp.push(tab);
     TargetTabs[url] = tmp;
     return(url);
+}
+
+function showTab(tab)
+{
+    console.log("Showing tab " + tab.url + " " + tab.id);
+    chrome.tabs.update(tab.id, {active: true});
+    chrome.windows.update(tab.windowId, {focused: true});
+}
+
+
+function muteTabs()
+{
+// adapted from https://github.com/danhp/mute-tab-chrome/blob/master/src/background.js
+    chrome.windows.getAll({populate: true},
+			  function(windowList) {
+			      windowList.forEach(function(window) {
+				  window.tabs.forEach(function(tab) {
+						if (tab.audible || tab.mutedInfo.muted) {
+							chrome.tabs.update(tab.id, {muted: true});
+						}
+					});
+				});
+			  });
+
+}
+
+
+function removeDuplicates()
+{
+   var tabsByURL = {};
+   chrome.windows.getAll({populate: true},
+			  function(windowList) {
+			      windowList.forEach(function(window) {
+				  window.tabs.forEach(function(tab) {
+				      var tmp = tabsByURL[ tab.url ];
+				      console.log("Adding " + tab.url);
+				      if(tmp == null)
+					  tmp = [];
+				      tmp.push(tab);
+				      tabsByURL[tab.url] = tmp;
+				      
+				  })
+			      })
+			      showDups(tabsByURL);
+			      closeTargetTabs(tabsByURL, true);
+			  });
+}
+
+function showDups(tabs)
+{
+    for(var k in tabs) {
+	if(tabs[k].length > 1)
+	    console.log("duplicate for " + k + " " + tabs[k].length);
+    }
 }
